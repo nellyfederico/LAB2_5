21d0bb223eddf06796865efe7232a6651d5ea532
nellyfederico <nallelyfedericoc@gmail.com>
2020-08-07 16:34:35 -0500

    LAB2_5

---
+++ LAB2_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LAB2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine("Enter a number:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int TotalResult = num1 + num2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(TotalResult);
+
+            TotalResult = num1 - num2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(TotalResult);
+
+
+            TotalResult = num1 * num2;
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(TotalResult);
+
+            double results2 = (double)num1 / (double)num2; 
+            double results3 = num1 % num2; 
+            Console.WriteLine("The answer is:");
+            Console.WriteLine(results2); 
+            Console.WriteLine(results3);
+        }
+    }
+}
